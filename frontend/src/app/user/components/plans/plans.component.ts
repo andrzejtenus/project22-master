3367a3333cd6b61ed71b2c8b988dab2153728a5a
andrzejtenus <32632343+andrzejtenus@users.noreply.github.com>
2020-10-21 08:25:10 +0200

    Revert "save changes"
    This reverts commit 0ca3573801a0f4114390789206d34fceb44637e2.

--- frontend/src/app/user/components/plans/plans.component.ts
+++ frontend/src/app/user/components/plans/plans.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {Plan, PlansService} from '../../../services/plans.service';
 import {DatePipe} from '@angular/common';
 import {Exercise, ExercisesService} from '../../../services/exercises.service';
-import {Observable} from 'rxjs';
 
 
 
@@ -16,8 +15,6 @@ export class PlansComponent implements OnInit {
 
   data: any;
 
-  currentPlan: Promise<Plan>;
-
   plan: Plan[];
   displayedColumns: string[] = ['Exercise', 'Sets', 'Reps', 'Weight', 'RPE'];
   liftTypes: string[] = ['MAIN_LIFT', 'SUPPORT_LIFT', 'ACCESSORY'];
@@ -28,9 +25,6 @@ export class PlansComponent implements OnInit {
 
   constructor(private plansService: PlansService, private datePipe: DatePipe, private exercisesService: ExercisesService) {
   }
-
-  currPlan: Plan;
-
   initPlan(): void
   {
     this.plansService.getPlanByDay(this.datePipe.transform(this.myDate, 'yyyy-MM-dd')).subscribe(value => {
@@ -64,7 +58,6 @@ export class PlansComponent implements OnInit {
 
   savePlan(exercise: string, day: Date, weight: number, sets: number, reps: number, rpe: number): void {
     this.plansService.addPlan(exercise, this.datePipe.transform(this.myDate, 'yyyy-MM-dd'), weight, sets, reps, rpe);
-    //console.log(this.currentPlan);
     this.initPlan();
     // DEBILIADA
     this.nextDay();
